f042252571172802b475a061cac57d9eeb1165da
AlexRamlogan02 <90638917+AlexRamlogan02@users.noreply.github.com>
2021-09-13 16:00:49 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.time.Year;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Objective is to calculate the year and age someone will retire
+        Scanner scan = new Scanner(System.in);
+        String inputAge;
+        long curAge, retAge, diff;
+        System.out.print("What is your current age?: ");
+        curAge = scan.nextLong();
+        System.out.print("What age do you want to retire?: ");
+        retAge = scan.nextLong();
+        diff = retAge - curAge;
+        Year curYear = Year.now(); //gets the current year
+        Year retYear = Year.now().plusYears(diff); //adds the amount of years needed to retire
+        System.out.print("You have " +diff +" years until you retire.\nIts " +curYear +" so you can retire in " +retYear);
+    }
+}
